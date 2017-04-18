b5cc97e96ea384f0641b04369902ebfd65f4c002
Abhijeet Prasad <abhijeetprasad98@gmail.com>
2017-04-18 16:51:22 -0400

    Finished novel component

--- src/Novel/Novel.js
+++ src/Novel/Novel.js
@@ -10,13 +10,13 @@ class Novel extends Component {
             <div className="novel pt-card">
                 <EditableText minWidth="100px" placeholder="Novel title" className="titletext" />
                 <div className="novel-edit">
-                    <Button type="reflexbutton" className="pt-button pt-minimal pt-icon-minus" />
+                    <Button type="reflexbutton" iconName="minus" className="pt-button pt-minimal pt-icon-large" />
                     <EditableText placeholder="0" className="numbertext" />
-                    <Button type="button" className="pt-button pt-minimal pt-icon-plus" />
+                    <Button type="button" iconName="plus" className="pt-button pt-minimal pt-icon-large"  />
                 </div>
                 <div className="icon-footer">
-                    <Button type="button" className="pt-button pt-minimal pt-icon-trash" />
-                    <Button type="button" className="pt-button pt-minimal pt-icon-star-empty" />
+                    <Button type="button" iconName="trash" className="pt-button pt-minimal" />
+                    <Button type="button" iconName="star-empty" className="pt-button pt-minimal" />
                 </div>
             </div>
         );
