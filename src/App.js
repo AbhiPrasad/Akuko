da1cf44824a838528468ddc5a199690582a0e7f6
Abhijeet Prasad <abhijeetprasad98@gmail.com>
2017-04-19 09:46:05 -0400

    Starting working on fnc

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import Novel from './Novel/Novel';
 
 import '../node_modules/@blueprintjs/core/dist/blueprint.css';
 import '../node_modules/normalize.css/normalize.css';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="app">
         Akuko
         <Novel />
       </div>
--- src/Novel/Novel.js
+++ src/Novel/Novel.js
@@ -5,14 +5,46 @@ import './Novel.css';
 import { EditableText, Button } from '@blueprintjs/core'
 
 class Novel extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            number: '0',
+            title: '',
+        }
+
+        this.addNum = this.addNum.bind(this);
+        this.minusNum = this.minusNum.bind(this);
+    }
+
+    addNum() {
+        console.log('wow');
+        
+    }
+
+    minusNum() {
+        console.log('whoa');
+    }
+
     render() {
+        const { number, title } = this.state
         return (
             <div className="novel pt-card">
-                <EditableText minWidth="100px" placeholder="Novel title" className="titletext" />
+                <EditableText minWidth="270px" placeholder="Novel title" className="titletext" />
                 <div className="novel-edit">
-                    <Button type="reflexbutton" iconName="minus" className="pt-button pt-minimal pt-icon-large" />
+                    <Button 
+                        onClick={() => this.minusNum()}
+                        type="reflexbutton" 
+                        iconName="minus" 
+                        className="pt-button pt-minimal pt-icon-large" 
+                    />
                     <EditableText placeholder="0" className="numbertext" />
-                    <Button type="button" iconName="plus" className="pt-button pt-minimal pt-icon-large"  />
+                    <Button 
+                        onClick={() => this.addNum()}
+                        type="button" 
+                        iconName="plus" 
+                        className="pt-button pt-minimal pt-icon-large"  
+                    />
                 </div>
                 <div className="icon-footer">
                     <Button type="button" iconName="trash" className="pt-button pt-minimal" />
